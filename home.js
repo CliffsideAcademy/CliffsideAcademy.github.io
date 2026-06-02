750943ed06c11b1dda9b9d3e495c34de7fb0a5c7
CliffsideAcademy <tylerdedic1@gmail.com>
2026-06-02 05:53:02 -0500

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,10 @@
+const panels = document.querySelectorAll(".panel");
 
+panels.forEach(panel => {
+  panel.classList.add("enhanced-panel");
+
+  const titles = panel.querySelectorAll("h2");
+  titles.forEach(t => {
+    t.classList.add("panel-title");
+  });
+});
